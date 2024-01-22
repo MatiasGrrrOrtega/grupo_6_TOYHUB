0f8656afc4603550018163d8a218237188eca728
Evelyn Ramos <eveh.rms7@gmail.com>
2024-01-22 17:19:19 -0300

    pages en funcionamiento

--- app.js
+++ app.js
@@ -10,25 +10,25 @@ app.get('/', (req, res) => {
   res.sendFile(filePath)
 })
 
-// app.get('/productDetail', (req, res) => {
-//   const filePath = path.join(__dirname, '/views/productDetail.html')
-//   res.sendFile(filePath)
-// })
+app.get('/productDetail', (req, res) => {
+const filePath = path.join(__dirname, '/views/productDetail.html')
+res.sendFile(filePath)
+})
 
-// app.get('/productCart', (req, res) => {
-//   const filePath = path.join(__dirname, '/views/productCart.html')
-//   res.sendFile(filePath)
-// })
+app.get('/productCart', (req, res) => {
+const filePath = path.join(__dirname, '/views/productCart.html')
+res.sendFile(filePath)
+})
 
-// app.get('/register', (req, res) => {
-//   const filePath = path.join(__dirname, '/views/register.html')
-//   res.sendFile(filePath)
-// })
+app.get('/register', (req, res) => {
+const filePath = path.join(__dirname, '/views/register.html')
+res.sendFile(filePath)
+})
 
-// app.get('/login', (req, res) => {
-//   const filePath = path.join(__dirname, '/views/login.html')
-//   res.sendFile(filePath)
-// })
+app.get('/login', (req, res) => {
+const filePath = path.join(__dirname, '/views/login.html')
+res.sendFile(filePath)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
