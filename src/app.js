fd8467a7e12be2631df3918f720807fe728aeeb9
MatiasOrtega <mat-1998-ort@hotmail.com>
2024-02-09 17:09:34 -0300

    Add method-override package for handling HTTP methods

--- src/app.js
+++ src/app.js
@@ -2,15 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
+const methodOverride = require('method-override')
 
-app.use(express.static('public'))
-// Indicamos el motor de plantilla
-app.set('view engine', 'ejs')
-//Direccionamos a las vistas
+app.use(express.static('public')) // Indicamos el motor de plantilla
+app.set('view engine', 'ejs') // Direccionamos a las vistas
 app.set('views', path.join(__dirname, '/views'))
-app.use(express.json())
-
+app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(methodOverride('_method')) // for put and delete methods
 
 const home = require('./routes/home')
 const user = require('./routes/user')
