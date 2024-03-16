c6f6bdaa7d4e2f78c527b1153a57c1c8e517b8b9
MatiasOrtega <mat-1998-ort@hotmail.com>
2024-03-16 17:31:26 -0300

    Formulario registro

--- src/controller/home.js
+++ src/controller/home.js
@@ -1,13 +1,13 @@
 const dataProducts = require('../data/products')
 const dataMarcas = require('../data/marcas')
-const USERS = require('../data/users')
+const dataUsers = require('../data/users')
 
 class homeController {
   static renderHome(req, res) {
     res.render('home', {
       products: dataProducts,
       marcas: dataMarcas.marcas,
-      usuario: USERS.users,
+      usuario: dataUsers,
     })
   }
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const router = express.Router()
-
+const multer = require('multer')
 const userController = require('../controller/user')
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/uploads/photosUsers')
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`)
+  },
+})
+
+const upload = multer({ storage })
 
 router.get('/login', userController.renderLogin)
 router.get('/register', userController.renderRegister)
+
+router.post('/', upload.single('photo-profile-user'), userController.createUser)
 
 module.exports = router
